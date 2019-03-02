357df429475d9d609840568d428f62c926f73d1b
tavo18 <golivieri@frsf.utn.edu.ar>
2019-03-02 13:39:36 -0300

    correct dataset and initial training code

---
+++ train_model.py
@@ -0,0 +1,41 @@
+import cv2
+import pickle
+import os.path
+import numpy as np
+from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten, Dense
+from helpers import resize_to_fit
+
+
+LETTER_IMAGES_FOLDER = "dataset/examples"
+MODEL_FILENAME = "captcha_model.hdf5"
+MODEL_LABELS_FILENAME = "model_labels.dat"
+
+
+# initialize the data and labels
+data = []
+labels = []
+
+# loop over the input images
+for image_file in paths.list_images(LETTER_IMAGES_FOLDER):
+    # Load the image and convert it to grayscale
+    image = cv2.imread(image_file)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Resize the letter so it fits in a 20x20 pixel box
+    image = resize_to_fit(image, 20, 20)
+
+    # Add a third channel dimension to the image to make Keras happy
+    image = np.expand_dims(image, axis=2)
+
+    # Grab the name of the letter based on the folder it was in
+    label = image_file.split(os.path.sep)[-2]
+    print(label)
+
+    # Add the letter image and it's label to our training data
+    data.append(image)
+    labels.append(label)
