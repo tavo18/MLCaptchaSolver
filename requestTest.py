88bb647cf5b08392cad75a46c5929f66960235c5
tavo18 <golivieri@frsf.utn.edu.ar>
2019-03-05 20:55:14 -0300

    requestTest.py with POST request

--- requestTest.py
+++ requestTest.py
@@ -4,9 +4,14 @@ import cv2
 
 FOLDER = "test_captcha/size8/"
 # image = cv2.imread(FOLDER+str(0)+".png")
-image = open(FOLDER+str(0)+".png", 'rb').read()
+
+image = open(FOLDER+str(0)+".png", 'rb')
+
+files = {
+    'file': ('image.png', image),
+}
 
 url = 'http://127.0.0.1:5000/'
 # params ={'query': image}
-response = requests.post(url, data=image)
+response = requests.post(url, files=files)
 print(response.content)
