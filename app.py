e9ca5c0bd9e855629337e7d25a1bf1c2987134f5
tavo18 <golivieri@frsf.utn.edu.ar>
2019-03-09 02:10:28 -0300

    Add model 4 into application

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from PIL import Image
 app = Flask(__name__)
 api = Api(app)
 
-MODEL_FILENAME = "captcha_model_8.hdf5"
+MODEL_FILENAME_8 = "captcha_model_8.hdf5"
+MODEL_FILENAME_4 = "captcha_model2.hdf5"
 MODEL_LABELS_FILENAME = "model_labels_8.dat"
 CAPTCHA_IMAGE_FOLDER = "test_captcha/size8" 
 
@@ -23,9 +24,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 with open(MODEL_LABELS_FILENAME, "rb") as f:
     lb = pickle.load(f)
 
-# Load the trained neural network
-model = load_model(MODEL_FILENAME)
+# Load the trained neural network model for 8sized captchas
+model = load_model(MODEL_FILENAME_8)
 model._make_predict_function()
+
+# Load the trained neural network model for 4sized captchas
+model4 = load_model(MODEL_FILENAME_4)
+model4._make_predict_function()
 
 # argument parsing
 parser = reqparse.RequestParser()
@@ -85,10 +90,61 @@ class PredictCaptcha8(Resource):
         
         return {'prediction': captcha_text}
 
+class PredictCaptcha4(Resource):
+    def post(self):
+        # use parser and find the user's query
+        pil_image = Image.open(request.files['file'])
+        image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+        # vectorize the user's query and make a prediction
+        letter_images = []
+        height,width,c = image.shape
+        
+
+        # 4 characters captchas:
+        letter_images.append(image[0:height,int((width/5)):int((width/5))*2])
+        letter_images.append(image[0:height,int((width/5))*2:int((width/5))*3])
+        letter_images.append(image[0:height,int((width/5))*3:int((width/5))*4])
+        letter_images.append(image[0:height,int((width/5))*4:100])
+
+        
+        # Create a list to hold our predicted letters
+        predictions = []
+        # predictions.append(image.shape)
+        # predictions.append(letter_images[0].shape)
+
+        # output = predictions
+
+        # loop over the letters
+        for letter_image in letter_images:
+
+
+
+        #     # Turn the single image into a 4d list of images to make Keras happy
+        #     # Add a third channel dimension to the image to make Keras happy
+        #     # letter_image = np.expand_dims(letter_image, axis=2)
+            letter_image = np.expand_dims(letter_image, axis=0)
+
+            
+
+        #     # Ask the neural network to make a prediction
+            prediction = model4.predict(letter_image)
+
+        #     # Convert the one-hot-encoded prediction back to a normal letter
+            letter = lb.inverse_transform(prediction)[0]
+            predictions.append(letter)
+
+        captcha_text = ''.join(predictions)
+        # # create JSON object
+        
+        
+        return {'prediction': captcha_text}
+
 
 # Setup the Api resource routing here
 # Route the URL to the resource
-api.add_resource(PredictCaptcha8, '/')
+api.add_resource(PredictCaptcha8, '/8')
+api.add_resource(PredictCaptcha4, '/4')
 
 
 if __name__ == '__main__':
